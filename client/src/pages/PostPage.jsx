c6e447506d23026db4a571c6bef410d14046a574
Op Pautu <pautuop@gmail.com>
2023-03-01 15:43:19 +0530

    Add single page post and some styling

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { formatISO9075 } from "date-fns";
 
 export default function PostPage() {
   const [postInfo, setPostInfo] = useState(null);
@@ -16,11 +17,17 @@ export default function PostPage() {
 
   return (
     <div className="post-page">
+      <h1>{postInfo.title}</h1>
+      <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+      <div className="author">by @{postInfo.author.username}</div>
       <div className="image">
         <img src={`http://localhost:3000/${postInfo.cover}`} alt="" />
       </div>
-      <h1>{postInfo.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: postInfo.content }}
+      />
     </div>
   );
 }
